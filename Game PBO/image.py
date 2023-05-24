a5d6cad4d94d9a91c850321b229dfd5b6c7a2a89
Antonius Munthe <117933233+antoniusmunthe@users.noreply.github.com>
2023-05-24 10:51:18 +0700

    Add files via upload

---
+++ Game PBO/image.py
@@ -0,0 +1,8 @@
+import pygame
+import os
+background = pygame.image.load(os.path.abspath("image/background.png"))
+bullet=pygame.image.load(os.path.join("image","bullet.png"))
+player=pygame.image.load(os.path.join("image","player.png"))
+troops=pygame.image.load(os.path.join("image","troops.png"))
+button=pygame.image.load(os.path.join("image","button.png"))
+troopsBoss=pygame.image.load(os.path.join("image","troopsBoss.png"))
